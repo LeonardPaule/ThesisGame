b0e188dc193e9e42b3b00731aa4e18f893b93804
Leonard Paule <leonardeli.paule.cics@ust.edu.ph>
2022-09-04 03:02:43 +0800

    text file maker
    to be able to make a txt file

--- Thesis/Assets/Scripts/ResultsHold.cs
+++ Thesis/Assets/Scripts/ResultsHold.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.IO;
 
 public class ResultsHold : MonoBehaviour
 {
@@ -14,20 +15,47 @@ public class ResultsHold : MonoBehaviour
     int time;
     int coins;
     int deaths;
+
+    string fileName;
+    string levelTxt;
+    string timeTxt;
+    string coinTxt;
+    string deathTxt;
+
     // Start is called before the first frame update
     void Start()
     {
+        Directory.CreateDirectory(Application.streamingAssetsPath + "/Gameplay_Results/");
+
         level = PassIt.level;
         time = PassIt.finalTime;
         coins = PassIt.finalCoins;
         deaths = PassIt.finalDeaths;
 
-        levelText.text = "Level: " + level + " completed";
-        timeText.text = "Time: " + time + " seconds";
-        coinText.text = "Coins: " + coins + " collected";
-        deathText.text = "Deaths: " + deaths + " times";
-    }
+        fileName = "Level " + level + " completed";
+        timeTxt = "Time: " + time + " seconds";
+        coinTxt = "Coins: " + coins + " collected";
+        deathTxt = "Deaths: " + deaths + " times";
 
+        levelText.text = fileName;
+        timeText.text = timeTxt;
+        coinText.text = coinTxt;
+        deathText.text = deathTxt;
+
+        CreateTextFile();
+    }
+    void CreateTextFile()
+    {
+        string txtDocumentName = Application.streamingAssetsPath + "/Gameplay_Results/" + fileName+".txt";
+        if (!File.Exists(txtDocumentName))
+        {
+            File.WriteAllText(txtDocumentName, fileName + "\n\n");
+        }
+        File.AppendAllText(txtDocumentName, 
+            timeTxt + "\n" +    
+            coinTxt + "\n" + 
+            deathTxt + "\n\n");
+    }
     // Update is called once per frame
     void Update()
     {
